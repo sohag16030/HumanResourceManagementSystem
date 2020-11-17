a591303a5ee0f8530287c23a6d67ca8ce07e1728
SohagSadaf <38885449+sohag16030@users.noreply.github.com>
2020-11-17 18:47:45 +0600

    ViewData

---
+++ HRM/HRM.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HRM.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRM.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public string Index()
+        {
+           return _employeeRepository.GetEmployee(1).Name;
+        }
+        public IActionResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            ViewData["PageTitle"] = "Employee Details";
+            ViewData["Employee"] = model;
+            return View();
+        }
+    }
+}
---
+++ HRM/HRM.Web/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRM.Web.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        public List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() {Id = 1 , Name = "mary" ,Email = "marry@gmail.com",Department ="HR"},
+                new Employee() {Id = 2 , Name = "jbhry" ,Email = "jbrry@gmail.com",Department ="SQA"},
+                new Employee() {Id = 3 , Name = "knkary" ,Email = "kknrry@gmail.com",Department ="DEVELOPMENT"},
+                new Employee() {Id = 4 , Name = "mojiry" ,Email = "mokjnry@gmail.com",Department ="HR"},
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
